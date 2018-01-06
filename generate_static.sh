5f97c6faac8d3f523c2e9cab018c8385b1df5439
Santiago Pastorino <spastorino@gmail.com>
2018-01-06 02:32:48 -0300

    Upgrade to Ghost 1.19.2 & add new NLL post

--- generate_static.sh
+++ generate_static.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 buster generate
-find static/ -type f -name '*.html' -o -name '*.txt' -o -name '*.rss' | xargs sed -i '' -e 's,http://localhost:2368,https://santiagopastorino.com,g'
-find static/ -type f -name '*.html' -o -name '*.txt' -o -name '*.rss' | xargs sed -i '' -e 's,http://fonts.googleapis.com,https://fonts.googleapis.com,g'
-find static/ -type f -name '*.html' -o -name '*.txt' -o -name '*.rss' | xargs sed -i '' -e 's,http://www.gravatar.com,https://www.gravatar.com,g'
-find static/ -type f -name '*.html' -o -name '*.txt' -o -name '*.rss' | xargs sed -i '' -e 's,http://code.jquery.com,https://code.jquery.com,g'
-find static/ -type f -name '*.html' -o -name '*.txt' -o -name '*.rss' | xargs sed -i '' -e 's,rss/index.html,rss/index.rss,g'
+find static/ -type f \( -name '*.html' -o -name '*.txt' -o -name '*.rss' \) -exec sed -i 's,http://localhost:2368,https://santiagopastorino.com,g' {} +
+find static/ -type f \( -name '*.html' -o -name '*.txt' -o -name '*.rss' \) -exec sed -i 's,http://fonts.googleapis.com,https://fonts.googleapis.com,g' {} +
+find static/ -type f \( -name '*.html' -o -name '*.txt' -o -name '*.rss' \) -exec sed -i 's,http://www.gravatar.com,https://www.gravatar.com,g' {} +
+find static/ -type f \( -name '*.html' -o -name '*.txt' -o -name '*.rss' \) -exec sed -i 's,http://code.jquery.com,https://code.jquery.com,g' {} +
+find static/ -type f \( -name '*.html' -o -name '*.txt' -o -name '*.rss' \) -exec sed -i 's,http://cloud.feedly.com,https://cloud.feedly.com,g' {} +
+find static/ -type f \( -name '*.html' -o -name '*.txt' -o -name '*.rss' \) -exec sed -i 's,rss/index.html,rss/index.rss,g' {} +
